10c06a3500b69f0c06a685fe91cd8bad082e870a
Sardo <sardo.ip@sardo.work>
2025-03-26 11:47:37 +0900

    Make dashboard demo

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,33 @@
 'use client';
-import Container from 'react-bootstrap/Container';
+import { Container, Card, Button, Image, Row, Col } from 'react-bootstrap';
 import Topbar from '../components/Topbar';
 import styles from "./page.module.css";
 
-
 function Dashboard() {
   return (
-    <div className={styles.page}>
+    <div className={styles.dashboard}>
       <Topbar />
       <main className={styles.main}>
         <Container>
-          <div className="latest-books">
-            
+          <h1>Dashboard</h1>
+          <div className={styles.dashboardCard}>
+            <Card>
+              <Card.Body>
+                <Card.Title><h2>Latest Book</h2></Card.Title>
+                <Row className={styles.latestBooks}>
+                  <Col md="2">
+                  <div className={styles.book}>
+                    <div className={styles.cover}>
+                      <Image className={styles.coverImg} src="/images/demo-cover1.jpg" thumbnail />
+                    </div>
+                    <div className="title">
+                      Hello World
+                    </div>
+                  </div>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
           </div>
         </Container>
       </main>
